2563959bfd0112fddfb82d65e6bcec5073b2afff
Edison Li <edison4354@gmail.com>
2022-03-02 16:07:55 -0800

    Minor file structure updates and inserting content to .tmpl file

--- makesite.go
+++ makesite.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
+	"os"
 )
 
+// Page holds all the information we need to generate a new
+// HTML page from a text file on the filesystem.
+type Page struct {
+	Paragraph string
+}
+
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	fileContents, err := ioutil.ReadFile("data/first-post.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(fileContents))
+
+	newFile, err := os.Create("new.html")
+	if err != nil {
+		panic(err)
+	}
+
+	page := Page{}
+
+	page.Paragraph = string(fileContents)
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	t.Execute(newFile, page)
+
 }
